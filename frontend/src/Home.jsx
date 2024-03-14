45126d0f50040397a4b330a4db60c3de0bf27754
Swarga-codes <sd3526@srmist.edu.in>
2024-03-14 14:33:37 +0530

    added styles

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,5 +1,5 @@
 
-import {useContext, useEffect, useState} from 'react'
+import {useContext, useState} from 'react'
 import './App.css'
 import { SocketContext } from './SocketContextApi'
 import {useNavigate} from 'react-router-dom'
@@ -18,9 +18,45 @@ function Home() {
 
   return (
     <>
-  <input type="email" placeholder='enter email id' value={email} onChange={(e)=>setEmail(e.target.value)}/>
-  <input type="text" placeholder='enter room id' value={roomId} onChange={(e)=>setRoomId(e.target.value)}/>
-  <button onClick={joinRoom}>Join Room</button>
+ <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="w-16 h-16 m-auto">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg>
+
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-white">Join the Video Call</h2>
+  </div>
+
+  <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form className="space-y-6" onSubmit={(e)=>{
+      e.preventDefault()
+      joinRoom()
+    }}>
+      <div>
+        <label htmlFor="email" className="block text-sm font-medium leading-6 text-white">Email address</label>
+        <div className="mt-2">
+          <input id="email" name="email" type="email" autoComplete="email" value={email} placeholder='Enter your email' onChange={(e)=>setEmail(e.target.value)} required className="block w-full p-2 rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <label htmlFor="room" className="block text-sm font-medium leading-6 text-white">Room ID</label>
+          
+        </div>
+        <div className="mt-2">
+          <input id="room" name="room" type="text" autoComplete="current-room" placeholder='Enter room ID' value={roomId} onChange={(e)=>setRoomId(e.target.value)} required className="block w-full p-2 rounded-md border-0 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Join Room</button>
+      </div>
+    </form>
+
+  
+  </div>
+</div>
     </>
   )
 }
--- frontend/src/Room.jsx
+++ frontend/src/Room.jsx
@@ -82,20 +82,29 @@ await peer.setLocalDescription(ans)
       },[socket, handleUserJoined, handleIncomingCall, handleCallAccepted, handleIncomingNegotiation, handleNegotitationFinal])
      
   return (
-    <div>
-   <h1>Room: {roomId} </h1>
-   <p>{remoteSocketId?'connected':'No one in the room'}</p>
-   {myStream && <button onClick={sendStreams}>Send Stream</button>}
-   {remoteSocketId && <button onClick={handleCallUser}>Call</button>}
-  {myStream && <>
-  <h1>My Stream</h1>
-  <ReactPlayer url={myStream}  muted playing width={300} height={300}/></>}
-  {remoteStream && <>
-  <h1>Remote Stream</h1>
-  <ReactPlayer url={remoteStream}  muted playing width={300} height={300}/></>}
-    <ul>
-     
-    </ul>
+    <div className='text-white p-10'>
+   <h1 className='text-2xl font-bold'>Room: {roomId} </h1>
+   <p className='mt-10 text-lg'>{remoteSocketId?'connected':'No one in the room'}</p>
+   <div className='mt-5'>
+   {myStream && <button className='p-2 bg-violet-600 rounded-md' onClick={sendStreams}>Send Stream</button>}
+   {remoteSocketId && <button className='p-2 bg-green-600 rounded-md ml-5' onClick={handleCallUser}>Call</button>}
+   </div>
+   <div className='flex mt-10 justify-between items-center'>
+  {myStream && <div>
+  <h1 className='font-bold text-center text-3xl'>Me</h1>
+  <ReactPlayer url={myStream}  muted playing width={500} height={400}/></div>}
+  {remoteStream && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="green" className="w-16 h-16">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+</svg>
+}
+  {remoteStream && <div>
+  <h1 className='font-bold text-center text-3xl'>Friend</h1>
+  <ReactPlayer url={remoteStream}  muted playing width={500} height={400}/></div>}
+  </div>
+  {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+</svg> */}
+
    </div>
   )
 }
